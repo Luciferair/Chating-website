0b32a239e217e2d90d924b2b062b44a485893a3d
Luciferair <purnimadolui115@gmail.com>
2024-04-02 23:12:03 +0530

    host html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://127.0.0.1:5500",
+        origin: "http://localhost:4000",
         methods: ["GET", "POST"]
     }
 });
