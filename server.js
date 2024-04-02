4f65759fde899f3026875625cca957338d1ff11f
Luciferair <purnimadolui115@gmail.com>
2024-04-02 23:12:22 +0530

    host html

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, {
+const io = require('socket.io')(http);/*(http, {
     cors: {
         origin: "http://localhost:4000",
         methods: ["GET", "POST"]
     }
-});
+});*/
+const path = require('path');
 const users = {};
 
-io.on('connection', socket =>{
-    socket.on('new-user-con', name =>{
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', socket => {
+    socket.on('new-user-con', name => {
         users[socket.id] = name;
         socket.broadcast.emit('user-con', name);
     });
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
-    socket.on('disconnect', message =>{
+    socket.on('disconnect', message => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 });
 
-
-http.listen(3000, ()=>{
-    console.log('server is runing');
+http.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
 });
